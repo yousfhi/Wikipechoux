a368de654084fa974562468c07f3d72435c15746
lturberg <154235336+lturberg@users.noreply.github.com>
2023-12-18 15:16:20 +0100

    Update vueEntete.php

--- vue/vueEntete.php
+++ vue/vueEntete.php
@@ -1,9 +1,17 @@
+<?php
+// vueEntete.php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
 <!DOCTYPE html>
 
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+        <!-- Enfin, incluez votre script suggestion.js -->
+        <script src="modele/suggestion.js"></script>
     <title></title>
 </head>
 <body>
@@ -25,6 +33,9 @@
                     <a class="nav-link" href="./?action=dico">Recherche par lettre</a>
                 </li>
                 <li class="nav-item">
+                    <a class="nav-link" href="./?action=rechercheMot">Recherche par mot</a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="./?action=theme">Recherche par theme</a>
                 </li>
                 <li class="nav-item">
@@ -34,7 +45,7 @@
                     <a class="nav-link" href="./?action=motAleatoire">Mot aléatoire</a>
                 </li>
                 <li class="nav-item">
-                    <a class="vue/vueFlashCode.php" href="./?action=flashCode">Flash code</a>
+                    <a class="nav-link" href="./?action=flashCode">Flash code</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="./?action=historique">Historique</a>
@@ -63,6 +74,21 @@
             </ul>
         </div>
 
+            <div class="container-fluid"></div>
+                <div class="row align-center">
+                    <div class="col-sx-12 col-md-12 col-lg-12">
+                        <form action="./?action=affichage" class="d-flex flex-row" method="POST">
+                        <input type="text" id="mot" name="mot" placeholder="Saisir un mot" class="form-control">
+                        </form>
+                        <!-- Container pour afficher les suggestions -->
+                        <form action="./?action=affichage" class="d-flex flex-row" method="POST">
+                        <div style="background-color: white;">
+                            <div id="suggestionsContainer" class="suggestions-container" style="display: block;"></div>
+                        </div>
+                        </form>
+                        <!-- Script pour gérer les suggestions -->
+                        <script src="modele/suggestion.js"></script>
+                    </div>
+                </div>
     </nav>
 
-    <div class="container-fluid"></div>
