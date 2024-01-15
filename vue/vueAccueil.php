53928d14eda156970161ff91980d9fd2cba6883b
zewixxx <84405746+zewixxx@users.noreply.github.com>
2024-01-15 14:47:41 +0100

    Update vueAccueil.php

--- vue/vueAccueil.php
+++ vue/vueAccueil.php
@@ -1,4 +1,13 @@
-<img src="image/Accueil.jpg" class="mainImage img-fluid" alt="">
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styleAccueil.css">
+    <title></title>
+</head>
+<div class="img">
+   <img src="image/Accueil.jpg" class="mainImage img-fluid" alt="">
+</div>
 <div class="content text-center">
    <div class="row align-center">
          <div class="col-sx-12 col-md-12 col-lg-12">
@@ -10,10 +19,14 @@
             
          </div>
       </div>
-     
+
    </div>
       
       
 </div>
-                  
+      
+    
+    
+        
+                
                
