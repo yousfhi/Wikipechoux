b42589d43f43c3acad8bb8ae6891d690f207a332
yousfhi <151522493+yousfhi@users.noreply.github.com>
2024-01-15 14:29:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $racine = dirname(__FILE__);
 include "$racine/controleur/Routeur.php";
 
 //Récupération de l'action à exécuter dans l'URL en méthode GET
-$action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
+$action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     if (!isset($action)){
         $action = "defaut";
     }
