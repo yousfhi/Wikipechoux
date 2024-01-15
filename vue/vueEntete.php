fffc52d9dd88a0bb3aa85b502923a608c001cdcd
zewixxx <84405746+zewixxx@users.noreply.github.com>
2024-01-15 14:47:21 +0100

    Update vueEntete.php

--- vue/vueEntete.php
+++ vue/vueEntete.php
@@ -5,90 +5,126 @@ if (session_status() == PHP_SESSION_NONE) {
 }
 ?>
 <!DOCTYPE html>
-
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-        <!-- Enfin, incluez votre script suggestion.js -->
-        <script src="modele/suggestion.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styleEntete.css">
     <title></title>
 </head>
 <body>
     
-    <nav class="navbar navbar-dark bg-dark" alt="vueEntete">
-        <!--Boutton Accueil-->
-        <a class="navbar-brand" href="./?action=defaut">Accueil</a>
-        <img src="image/CouvertureEntete.jpg" alt="imgEntete" class="img-responsive">
+<div class="titre">
+        <p>WIKIPECHOUX</p>
 
-
-        <!--Boutton coulissant-->
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarEntete" aria-controls="navbarEntete" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarEntete">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="./?action=dico">Recherche par lettre</a>
+    </div>
+    <nav>
+        <label for="toggle">☰</label>
+        <input type="checkbox" id="toggle" class="checkbox1">
+        <div class="main_pages">
+            <div class="espace"></div>
+        <li class="nav-item">
+                    <a class="nav-link" href="./?action=acceuil">Acceuil</a>
                 </li>
+
+                <div class="barre-grise"></div>
                 <li class="nav-item">
                     <a class="nav-link" href="./?action=rechercheMot">Recherche par mot</a>
                 </li>
+            
+                <div class="barre-grise"></div>
                 <li class="nav-item">
                     <a class="nav-link" href="./?action=theme">Recherche par theme</a>
                 </li>
+                <div class="barre-grise"></div>
                 <li class="nav-item">
                     <a class="nav-link" href="./?action=madeleines">Madeleine</a>
                 </li>
+                <div class="barre-grise"></div>
                 <li class="nav-item">
                     <a class="nav-link" href="./?action=motAleatoire">Mot aléatoire</a>
                 </li>
+                <div class="barre-grise"></div>
                 <li class="nav-item">
                     <a class="nav-link" href="./?action=flashCode">Flash code</a>
                 </li>
+                <div class="barre-grise"></div>
                 <li class="nav-item">
                     <a class="nav-link" href="./?action=historique">Historique</a>
-                </li>
+                </li> 
+                <div class="barre-grise"></div>
                 <br> 
-                
                 
                 <?php
                     if(isset($_SESSION['login'])){
                 ?>
-                
+                <div class="espace2"></div>
+                <div class="barre-grise"></div>
                 <li class="nav-item">
                     <a href="./?action=mesInfos" class="nav-link">Mes informations</a>
                 </li>
                 
-                
+                <div class="barre-grise"></div>
                 <li class="nav-item">
                     <a class="nav-link" href="./?action=login&type=out">Se déconnecter</a>
                 </li>
+                <div class="barre-grise"></div>
                 <?php }else{?>
                 <li class="nav-item">
                     <a class="nav-link" href="./?action=login&type=in">Se connecter</a>
                 </li>
+
+                
+                
+                
                 <?php }?>
 
             </ul>
-        </div>
 
-            <div class="container-fluid"></div>
-                <div class="row align-center">
-                    <div class="col-sx-12 col-md-12 col-lg-12">
-                        <form action="./?action=affichage" class="d-flex flex-row" method="POST">
-                        <input type="text" id="mot" name="mot" placeholder="Saisir un mot" class="form-control">
-                        </form>
-                        <!-- Container pour afficher les suggestions -->
-                        <form action="./?action=affichage" class="d-flex flex-row" method="POST">
-                        <div style="background-color: white;">
-                            <div id="suggestionsContainer" class="suggestions-container" style="display: block;"></div>
-                        </div>
-                        </form>
-                        <!-- Script pour gérer les suggestions -->
-                        <script src="modele/suggestion.js"></script>
-                    </div>
+            
+
+
+ </div>
+
+ <div class="mid">
+  <div class="row">
+      <div class="col-sx-12 col-md-12 col-lg-12">
+          <form id="searchForm" action="./?action=affichage" class="d-flex flex-row" method="POST">
+              <input type="text" name="mot" placeholder="Saisir un mot..." class="form-control" onfocus="clearPlaceholder(this)">
+                  <img src="loupe-icon.png" alt="Search Icon" class="search-icon">
+              </button>
+          </form>
+      </div>
+  </div>
+</div>
+
+
+
+<script>document.addEventListener('DOMContentLoaded', function() {
+  var searchInput = document.querySelector('#searchForm input[type="text"]');
+  var searchIcon = document.querySelector('#searchForm .search-icon');
+
+  searchIcon.addEventListener('click', function() {
+    searchInput.classList.toggle('show');
+  });
+});
+</script>
+
+
+  <script>
+    function clearPlaceholder(input) {
+      input.placeholder = '';
+    }
+  </script>
+        <div style=" left: 50%; transform: translate(-50%); display: inline-table;">
+            <form action="./?action=affichage" class="d-flex flex-row" method="POST">
+                <input type="text" id="mot" name="mot" placeholder="Saisir un mot" class="form-control">
+            </form>
+            <form action="./?action=affichage" class="d-flex flex-row" method="POST">
+                <div style="background-color: white;">
+                    <div id="suggestionsContainer" class="suggestions-container" style="display: block;"></div>
                 </div>
+            </form>
+            <script src="modele/suggestion.js"></script>
+        </div>
     </nav>
 
