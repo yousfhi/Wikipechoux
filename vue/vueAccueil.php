ab24cdae0aaba8816ca2bf042a6858be7fe92c29
yousfhi <151522493+yousfhi@users.noreply.github.com>
2024-01-15 14:32:55 +0100

    Update vueAccueil.php

--- vue/vueAccueil.php
+++ vue/vueAccueil.php
@@ -10,17 +10,10 @@
             
          </div>
       </div>
-      <div class="row align-center">
-         <div class="col-sx-12 col-md-12 col-lg-12">
-            <form action="./?action=affichage" class="d-flex flex-row" method="POST"> 
-               <input type="text" name="mot" placeholder="Saisir un mot" class="form-control">
-               <input type="submit" value="=>" class="btn btn-success" >
-            </form>
-         </div>
-      </div>
+     
    </div>
       
       
 </div>
-                
+                  
                
