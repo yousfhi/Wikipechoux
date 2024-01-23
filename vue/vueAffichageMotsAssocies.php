d45f2cf59585db584b91064fea9f739aa4226f7f
lturberg <154235336+lturberg@users.noreply.github.com>
2024-01-23 13:46:22 +0100

    Update vueAffichageMotsAssocies.php

--- vue/vueAffichageMotsAssocies.php
+++ vue/vueAffichageMotsAssocies.php
@@ -1,1 +1,13 @@
-
+<!-- Fichier : vue/vueAffichageMotsAssocies.php -->
+<?php if ($motsAssocies && is_array($motsAssocies)): ?>
+  <div>
+    <h3>Mots associés :</h3>
+    <ul>
+      <?php foreach ($motsAssocies as $motAssocie): ?>
+        <li><?= $motAssocie->getLibelle() ?></li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
+<?php else: ?>
+  <div>Aucun mot associé trouvé.</div>
+<?php endif; ?>
