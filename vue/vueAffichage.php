9028c97a7ab79678454e863f90b7d3db370ea83a
lturberg <154235336+lturberg@users.noreply.github.com>
2024-01-23 09:46:31 +0100

    Update vueAffichage.php

--- vue/vueAffichage.php
+++ vue/vueAffichage.php
@@ -1,81 +1,68 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Affichage Mot</title>
-    <link rel="stylesheet" href=".\css\style.css">
-  </head>
-  <body>
     <?php
-    if($estTrouve){
+    if ($estTrouve) {
     ?>
-      
-        <center><h1><?=$unMot->getLibelle()?></h1> </center> 
-        <center><h2><?=$unMot->getDefinition()?></h2> </center>
+        <!-- Display the word information centered -->
+        <center><h1><?= $unMot->getLibelle() ?></h1></center> 
+        <center><h2><?= $unMot->getDefinition() ?></h2></center>
     <?php
 
-        if(ModeleMotDAO::isPhoto($unMot->getId())){
+        // Check if the word has a photo
+        if (ModeleMotDAO::isPhoto($unMot->getId())) {
       
-      ?>
-      
-
-      
+    ?>
+            <!-- Display a slider for photos -->
             <div class="slider-container">
-            <div class="menu">
-            <?php
-            
-            for($i = 0; $i < count($lesPhotos); $i++){
-                echo "<label for='slide-dot-".($i+1)."'></label>";
-            }
-        ?>
-          
-        
-            </div>
+                <div class="menu">
+                    <?php
+                        // Generate labels for each photo slide
+                        for ($i = 0; $i < count($lesPhotos); $i++) {
+                            echo "<label for='slide-dot-" . ($i+1) . "'></label>";
+                        }
+                    ?>
+                </div>
       
-        <?php
-            for($i = 0;$i < count($lesPhotos); $i++){
-            echo '<input class="slide-input" id="slide-dot-'.($i+1).'" type="radio" name="slides" checked>' ;
-            echo '<img class="slide-img" src="./image/'.$lesPhotos[$i]['fichier'].'">' ;
-            }
-        ?>
-      
-      
+                <?php
+                    // Generate radio buttons and images for each photo slide
+                    for ($i = 0; $i < count($lesPhotos); $i++) {
+                        echo '<input class="slide-input" id="slide-dot-' . ($i+1) . '" type="radio" name="slides" checked>';
+                        echo '<img class="slide-img" src="./image/' . $lesPhotos[$i]['fichier'] . '">';
+                    }
+                ?>
             </div>
 
-            
-        <div>
-          <?php
+            <!-- Display links for the previous and next words -->
+            <div>
+                <?php
+                    // Get the IDs for the previous and next words
+                    $idMotPrecedent = ModeleMotDAO::getMotPrecedent($idMot)->getId();
+                    $idMotSuivant = ModeleMotDAO::getMotSuivant($idMot)->getId();
 
-
-            $idMotPrecedent = ModeleMotDAO::getMotPrecedent($idMot)->getId();
-            $idMotSuivant = ModeleMotDAO::getMotSuivant($idMot)->getId();
-
-              echo "<a href='./?action=affichagel&mot=$idMotPrecedent'>Mot Précédent</a>";
-
-              echo "<a href='./?action=affichagel&mot=$idMotSuivant'>Mot Suivant</a>";
-              
-
-          ?>
-        </div>
-      <?php
+                    // Display links for the previous and next words
+                    echo "<a href='./?action=affichage&mot=$idMotPrecedent'>Mot Précédent</a>";
+                    echo "<a href='./?action=affichage&mot=$idMotSuivant'>Mot Suivant</a>";
+                ?>
+            </div>
+    <?php
         }
-            $idMotPrecedent = ModeleMotDAO::getMotPrecedent($idMot)->getId();
-            $idMotSuivant = ModeleMotDAO::getMotSuivant($idMot)->getId();
-            $motSuivantApercu = ModeleMotDAO::getMotSuivantApercu($idMot);
-              echo "<a href='./?action=affichagel&mot=$idMotPrecedent'>Mot Précédent</a>";
 
-              echo "<a href='./?action=affichagel&mot=$idMotSuivant'>Mot Suivant($motSuivantApercu)</a>";
-
-
-      
-    }
-      
-      
-    else{
-      ?>
-    <div> Mot inconnu </div>
+        $idMotPrecedent = ModeleMotDAO::getMotPrecedent($idMot)->getId();
+        $idMotSuivant = ModeleMotDAO::getMotSuivant($idMot)->getId();
+        $motSuivantApercu = ModeleMotDAO::getMotSuivantApercu($idMot);
+        echo '<div style="display: flex;">';
+        echo '<div>';
+        echo '<button onclick="window.location.href=\'./?action=affichage&mot=' . $idMotPrecedent . '\'">Mot Précédent</button>';
+        echo '</div>';
+        
+        echo '<div>';
+        echo '<button onclick="window.location.href=\'./?action=affichage&mot=' . $idMotSuivant . '\'">Mot Suivant ' . implode(",<br>\n", $motSuivantApercu) . '</button>';
+        echo '</div>';
+        echo '</div>';
+        
+        
+      }
+    else {
+    ?>
+        <!-- Display "Mot inconnu" if $estTrouve is false -->
+        <div> Mot inconnu </div>
     <?php
       }
-      ?>
-        
-  </body>
-</html>
