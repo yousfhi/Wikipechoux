960a99cfad271138d6e3232437d673e4009920b2
lturberg <154235336+lturberg@users.noreply.github.com>
2024-01-26 09:46:37 +0100

    Delete controleur/ctrlInfo.php

--- controleur/ctrlInfo.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if(isset($_SESSION['login'])){
-        
-        include "$racine/vue/vueEntete.php";
-        include "$racine/vue/vueInfo.php";
-        include "$racine/vue/vuePied.php";
-    }
