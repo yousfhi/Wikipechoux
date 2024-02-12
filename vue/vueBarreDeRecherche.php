5101b59fcac2d6edf85e663918ab46d7dfa6f8f1
zewixxx <84405746+zewixxx@users.noreply.github.com>
2024-02-12 09:51:33 +0100

    Update vueBarreDeRecherche.php

--- vue/vueBarreDeRecherche.php
+++ vue/vueBarreDeRecherche.php
@@ -1,15 +1,1 @@
-<!-- barre de recherche avec suggestion -->
-<footer>
-    <div style="width: 100%;">
-        <form action="./?action=affichage" class="d-flex flex-column" method="POST">
-            <input type="text" id="mot" name="mot" placeholder="&#128269 Recherche" class="barreDeRecherche">
-            
-            <div style="background-color: white; width: 100%;">
-                <div id="suggestionsContainer" class="suggestions-container" style="display: block;"></div>
-            </div>
 
-            <button type="submit" class="btnRechercher">Rechercher</button>
-        </form>
-        <script src="modele/suggestion.js"></script>
-    </div>
-</footer>
