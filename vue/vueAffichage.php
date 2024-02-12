ec9c45cbf357fc14739158ccc714c021b8a89c31
zewixxx <84405746+zewixxx@users.noreply.github.com>
2024-02-12 15:16:28 +0100

    Update vueAffichage.php

--- vue/vueAffichage.php
+++ vue/vueAffichage.php
@@ -1,4 +1,13 @@
-<div class="contenuAffichage" style="background-color: lightgray">
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styleAffichageMot.css">
+    <title></title>
+</head>
+
+
+<div class="contenuAffichage">
 
   <?php
   if ($estTrouve) {
