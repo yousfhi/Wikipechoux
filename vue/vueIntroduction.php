d85c08bd8f98939a6e39d49602dcd9e442bc7ccd
zewixxx <84405746+zewixxx@users.noreply.github.com>
2024-02-12 13:41:09 +0100

    Update vueIntroduction.php

--- vue/vueIntroduction.php
+++ vue/vueIntroduction.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styleIntroduction.css">
+    <title></title>
+</head>
 <div class="contenuIntroduction">
     <div class="container">
         <header>
