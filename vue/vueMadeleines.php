b9eb93397dda4f287a0379849130e248885569b2
zewixxx <84405746+zewixxx@users.noreply.github.com>
2024-02-12 14:11:27 +0100

    Update vueMadeleines.php

--- vue/vueMadeleines.php
+++ vue/vueMadeleines.php
@@ -1,4 +1,13 @@
-    <div class="contenuMadelaines" style="background-color: lightgray">
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styleMadeleines.css">
+    <title></title>
+</head>   
+   
+   
+   <div class="contenuMadelaines" >
             <div>
                 <h3>
                     <center>
