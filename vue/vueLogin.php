38e9f966da1ba47bf33af8919885e1ab16ffb1e0
zewixxx <84405746+zewixxx@users.noreply.github.com>
2024-02-12 15:11:21 +0100

    Update vueLogin.php

--- vue/vueLogin.php
+++ vue/vueLogin.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styleLogin.css">
+    <title></title>
+</head>
+
 <div class="contenuLogin">
 
         <div class="container">
