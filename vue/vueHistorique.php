d63f7cb8f871e3475a2ed0e095f4fa1057473c3c
zewixxx <84405746+zewixxx@users.noreply.github.com>
2024-02-12 15:09:07 +0100

    Update vueHistorique.php

--- vue/vueHistorique.php
+++ vue/vueHistorique.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styleHistorique.css">
+    <title></title>
+</head>
+
 <div class="contenuHistorique" style="background-color: lightgray">
     <div class="row">
         <div class="col-4 mx-auto text-center">
